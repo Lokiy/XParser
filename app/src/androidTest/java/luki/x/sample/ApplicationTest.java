efc8f1ecc2da0a7853119ee19a99f70c9b62bc65
刘龙克 <liulongke@msn.cn>
2016-01-12 15:19:56 +0800

    V1.1.8.1

---
+++ app/src/androidTest/java/luki/x/sample/ApplicationTest.java
@@ -0,0 +1,20 @@
+package luki.x.sample;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+
+	public ApplicationTest() {
+		super(Application.class);
+	}
+
+	public void testEqual() throws Exception {
+		final int expected = 1;
+		final int reality = 1;
+		assertEquals(expected, reality);
+	}
+}
